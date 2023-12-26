dbd6ef9cc3d5b51a6e306b87b28122a348067cd5
vishal <39182918+imvishal09@users.noreply.github.com>
2023-12-26 23:54:16 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
-const projects = [
-  {
-    name: 'Tableau-Dashboard',
-    image: 'https://raw.githubusercontent.com/imvishal09/Tableau-Dashboard/main/OVERVIEW.png',
-    description: 'A detailed Tableau dashboard visualizing various datasets.',
-    link: 'https://github.com/imvishal09/Tableau-Dashboard',
-    author: 'Vishal'
-  },
-  // ... other projects
-];
+document.addEventListener('DOMContentLoaded', function() {
+    const projects = [
+        {
+            title: "International Artist Feature: Malaysia",
+            author: "Mary Winkler",
+            imageUrl: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg",
+            link: "https://design.tutsplus.com/articles/international-artist-feature-malaysia--cms-26852",
+            description: "A feature on Malaysian artists."
+        },
+        // Add more projects here
+    ];
 
-const container = document.getElementById('projects-container');
+    const container = document.querySelector('.band');
 
-projects.forEach(project => {
-  const card = document.createElement('div');
-  card.className = 'item';
-  
-  const cardLink = document.createElement('a');
-  cardLink.href = project.link;
-  cardLink.className = 'card';
-  
-  const thumb = document.createElement('div');
-  thumb.className = 'thumb';
-  thumb.style.backgroundImage = `url(${project.image})`;
-  
-  const article = document.createElement('article');
-  
-  const title = document.createElement('h1');
-  title.textContent = project.name;
-  
-  const author = document.createElement('span');
-  author.textContent = project.author;
-  
-  const description = document.createElement('p');
-  description.textContent = project.description;
-  
-  article.appendChild(title);
-  article.appendChild(description);
-  article.appendChild(author);
-  
-  cardLink.appendChild(thumb);
-  cardLink.appendChild(article);
-  
-  card.appendChild(cardLink);
-  
-  container.appendChild(card);
+    projects.forEach(project => {
+        const cardElement = createProjectCard(project);
+        container.appendChild(cardElement);
+    });
+
+    function createProjectCard(project) {
+        const card = document.createElement('a');
+        card.className = 'card';
+        card.href = project.link;
+        card.target = "_blank"; // Opens link in a new tab
+
+        const thumb = document.createElement('div');
+        thumb.className = 'thumb';
+        thumb.style.backgroundImage = `url(${project.imageUrl})`;
+
+        const article = document.createElement('article');
+        const title = document.createElement('h1');
+        title.textContent = project.title;
+        const author = document.createElement('span');
+        author.textContent = project.author;
+
+        article.appendChild(title);
+        article.appendChild(author);
+        card.appendChild(thumb);
+        card.appendChild(article);
+
+        return card;
+    }
 });
