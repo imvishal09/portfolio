bf3da634f501e2577e78d63b74476ff1a989be96
vishal <39182918+imvishal09@users.noreply.github.com>
2024-01-03 00:29:42 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const projects = [
-        {
-            title: "Tableau Dashboard",
-            imageUrl: "https://raw.githubusercontent.com/imvishal09/Tableau-Dashboard/main/OVERVIEW.png",
-            link: "https://github.com/imvishal09/Tableau-Dashboard",
-            description: "US Consumer Complaints"
-        },
-         {
-            title: "Netflix stock trend",
-            imageUrl: "https://raw.githubusercontent.com/imvishal09/Netflix-Stock-Price-Trend-Prediction/main/Ouput.png",
-            link: "https://github.com/imvishal09/Netflix-Stock-Price-Trend-Prediction",
-            description: "Prediction of Netflix stock."
-        },
-         {
-            title: "Image Classification",
-            imageUrl:"",
-            link: "https://github.com/imvishal09/image-classification",
-            description: "Image classification using ML."
-        }
+        // ... your project objects
     ];
 
     const container = document.querySelector('.band');
@@ -35,19 +18,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const thumb = document.createElement('div');
         thumb.className = 'thumb';
-        thumb.style.backgroundImage = `url(${project.imageUrl})`;
+        if (project.imageUrl) {
+            thumb.style.backgroundImage = `url(${project.imageUrl})`;
+        }
 
         const article = document.createElement('article');
         const title = document.createElement('h1');
         title.textContent = project.title;
-        const author = document.createElement('span');
-        author.textContent = `By ${project.author}`;
         const description = document.createElement('p');
         description.textContent = project.description;
 
         article.appendChild(title);
         article.appendChild(description);
-        article.appendChild(author);
         card.appendChild(thumb);
         card.appendChild(article);
 
